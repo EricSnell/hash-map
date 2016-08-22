f4f03fba9943d68af74c897dba65d8d240113b29
jbenjaminy <young.benjamin@gmail.com>
2016-08-22 11:49:57 -0500

    adds interview-questions.js

---
+++ interview-questions.js
@@ -0,0 +1,3 @@
+/* Write an algorithm to check whether any permutation of a string is a palindrome (a string which reads the same forwards and backwards). For example, "madam", "amadm" and "cllci" should all return true, whereas "caabl" and "aaxxis" should return false. */
+/* Write an algorithm to group a list of words into anagrams. For example, if the input was ['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race'], the output should be: [['east', 'teas', 'eats'], ['cars', 'arcs'], ['acre', 'race']]. */
+/* Write a hash map implementation which uses separate chaining. */
