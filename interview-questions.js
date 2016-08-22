0960e2ece1754ade242ce51d98d1f1e8d2ed5847
jbenjaminy <young.benjamin@gmail.com>
2016-08-22 16:29:29 -0500

    finishes interview question 2

--- interview-questions.js
+++ interview-questions.js
@@ -2,12 +2,12 @@
 var HashMap = require('./hash-exercise');
 /* Write an algorithm to check whether any permutation of a string is a palindrome (a string which reads the same forwards and backwards). For example, "madam", "amadm" and "cllci" should all return true, whereas "caabl" and "aaxxis" should return false. */
 let palinCheck = (string) => {
-	let anticipatedMatches = string.length
+	let antMatches = string.length
 	let matches = 0
 	if (string.length % 2 !== 0) {
-		anticipatedMatches = anticipatedMatches - 1;
+		antMatches = antMatches - 1;
 	}
-	anticipatedMatches = anticipatedMatches / 2
+	antMatches = antMatches / 2
 	for (var i = 0; i < string.length; i++) {
 		for (var j = i + 1; j < string.length; j++) {
 			if (string[i] === string[j]) {
@@ -17,20 +17,20 @@ let palinCheck = (string) => {
 			}
 		}
 	}
-	console.log(anticipatedMatches, '<-- anticipatedMatches', matches, '<-- matches')
-	return matches === anticipatedMatches
+	console.log(antMatches + ' | ' + matches + ' (antMatches | matches)')
+	return matches === antMatches
 }
 
 function demo(str) {
 	console.log(str, palinCheck(str) ? "is palindromable" : "is not palindromable");
 }
-/*
+
 demo('o') 	// true	
 demo('ppp')		// true
 demo('pppaaa')	// false
 demo('popopddplll')	// true
-demo(Array(50).join())  // true
-*/
+demo(Array(20).join())  // true
+
 
 /* Write an algorithm to group a list of words into anagrams. For example, if the input was ['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race'], the output should be: [['east', 'teas', 'eats'], ['cars', 'arcs'], ['acre', 'race']]. */
 
@@ -38,7 +38,6 @@ let anagrams = (array) => {
 	let anagramMap = new HashMap()
 	let outputArr = []
 	for (var i = 0; i < array.length; i++) {
-		console.log(array[i])
 		let sorted = array[i].split('').sort().join('')
 
 		try {
@@ -48,12 +47,13 @@ let anagrams = (array) => {
 			anagramMap.set(sorted, [array[i]])
 		}
 	}
-	console.log(anagramMap._slots);
-	// for (var key in anagramMap) {
-	// 	outputArr.push(anagramMap[key])
-	// }
-
-	// return outputArr
+	let mapArr = anagramMap._slots
+	for (var slot in mapArr) {
+		if (slot) {
+			outputArr.push(anagramMap._slots[slot].value)
+		} 
+	}
+	return outputArr
 }
 
 console.log(anagrams(['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race']))
